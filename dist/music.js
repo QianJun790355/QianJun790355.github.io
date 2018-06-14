90a534e3cb8c95d5e377dff9b8c5766dca1f55a4
QianJun790355 <790355031@qq.com>
2018-06-14 16:31:01 +0800

    Site updated: 2018-06-14 16:31:00

--- dist/music.js
+++ dist/music.js
@@ -16,12 +16,6 @@ const ap = new APlayer({
         cover: 'http://oeff2vktt.bkt.clouddn.com/image/8.jpg',
       },
       {
-        name: "张韶涵&雨馨雨薇 - 淋雨一直走+隐形的翅膀",
-        artist: '张韶涵',
-        url:  'http://up.mcyt.net/?down/39594.mp3,
-        cover: 'http://oeff2vktt.bkt.clouddn.com/image/52.jpg,
-      },
-      {
         name: '风筝误',
         artist: '刘珂矣',
         url: 'http://up.mcyt.net/?down/46644.mp3',
