e59aefb149725c144db33fc4136661398af51619
QianJun790355 <790355031@qq.com>
2018-06-14 16:22:57 +0800

    Site updated: 2018-06-14 16:22:53

---
+++ dist/music.js
@@ -0,0 +1,31 @@
+const ap = new APlayer({
+    container: document.getElementById('aplayer'),
+    fixed: true,
+    autoplay: false,
+    audio: [
+      {
+        name: "PDD洪荒之力",
+        artist: '徐梦圆',
+        url: 'http://up.mcyt.net/?down/39868.mp3',
+        cover: 'http://oeff2vktt.bkt.clouddn.com/image/84.jpg',
+      },
+      {
+        name: '9420',
+        artist: '麦小兜',
+        url: 'http://up.mcyt.net/?down/45967.mp3',
+        cover: 'http://oeff2vktt.bkt.clouddn.com/image/8.jpg',
+      },
+      {
+        name: "张韶涵&雨馨雨薇 - 淋雨一直走+隐形的翅膀",
+        artist: '张韶涵',
+        url:  'http://up.mcyt.net/?down/39594.mp3,
+        cover: 'http://oeff2vktt.bkt.clouddn.com/image/52.jpg,
+      },
+      {
+        name: '风筝误',
+        artist: '刘珂矣',
+        url: 'http://up.mcyt.net/?down/46644.mp3',
+        cover: 'http://oeff2vktt.bkt.clouddn.com/image/96.jpg',
+      }
+    ]
+});
