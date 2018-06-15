39b714449d2f587139d5d2bb2b93321e32f72dab
QianJun790355 <790355031@qq.com>
2018-06-15 09:27:32 +0800

    Site updated: 2018-06-15 09:27:24

--- dist/music.js
+++ dist/music.js
@@ -4,6 +4,18 @@ const ap = new APlayer({
     autoplay: false,
     audio: [
       {
+        name: "淋雨一直走",
+        artist: '张韶涵',
+        url: 'http://up.mcyt.net/?down/36469.mp3',
+        cover: 'http://oeff2vktt.bkt.clouddn.com/image/83.jpg',
+      },
+       {
+        name: "偏爱",
+        artist: '张芸京',
+        url: 'http://up.mcyt.net/?down/47747.mp3',
+        cover: 'http://oeff2vktt.bkt.clouddn.com/image/31.jpg',
+      },
+      {
         name: "PDD洪荒之力",
         artist: '徐梦圆',
         url: 'http://up.mcyt.net/?down/39868.mp3',
@@ -22,4 +34,4 @@ const ap = new APlayer({
         cover: 'http://oeff2vktt.bkt.clouddn.com/image/96.jpg',
       }
     ]
-});
+})
